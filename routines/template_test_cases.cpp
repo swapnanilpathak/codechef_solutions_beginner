30e335f17dd10a003f5b4721c149d8bfff4ac23b
swapnanilpathak <42994624+swapnanilpathak@users.noreply.github.com>
2019-01-11 01:23:35 +0530

    Template for T test cases scenarios

---
+++ routines/template_test_cases.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string.h>
+#include<math.h>
+#include<algorithm>
+#include <stack>
+
+#include<vector>
+typedef long long int lli;
+typedef long long ll;
+
+
+using namespace std;
+int main(){
+	
+	int T=0;
+	cin>>T;
+	while(T--){
+		
+		
+		
+		
+	}
+	
+	return 0;
+}
